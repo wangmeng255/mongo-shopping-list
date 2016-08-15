f0bddafa920ea4d6d56532d6eabf558abbf9edbd
Meng Wang <wangmeng255@gmail.com>
2016-08-15 19:07:36 +0000

    change config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL || 
                        (process.env.NODE_ENV === 'production' ? 
                        //'mongodb://localhose/shopping-list' : 
-                       'mongodb://<wangmeng255>:<SoundwaveLi00>@ds139985.mlab.com:39985/mongo_data/shopping-list':
+                       'mongodb://wangmeng255:SoundwaveLi00@ds139985.mlab.com:39985/mongo_data':
                        'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
